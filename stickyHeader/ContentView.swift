6fae433679460bd7be48e3dcc79e866735b0a903
whatchuThinkin <spearhead0427@gmail.com>
2022-11-09 14:15:16 +0900

    initial

---
+++ stickyHeader/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  stickyHeader
+//
+//  Created by 1100690 on 2022/11/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ stickyHeader/stickyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  stickyHeaderApp.swift
+//  stickyHeader
+//
+//  Created by 1100690 on 2022/11/09.
+//
+
+import SwiftUI
+
+@main
+struct stickyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
